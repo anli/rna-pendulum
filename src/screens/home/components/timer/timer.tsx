5f62944284f38270e47176ecd3bab47d8aa9d261
Su Anli <sual0001@gmail.com>
2020-09-26 07:24:41 +0000

    fix(home): change timer ux

---
+++ src/screens/home/components/timer/timer.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Colors, IconButton} from 'react-native-paper';
+import styled from 'styled-components/native';
+import getMinSec from './get-min-sec';
+
+const Timer = ({status, onPress, countdown, testID}: any) => {
+  const isEnabled = status === 'FULL';
+  return (
+    <TimerContainer onTouchEnd={() => isEnabled && onPress()}>
+      <Hourglass
+        testID={`${testID}.Hourglass`}
+        icon={getIconByStatus(status)}
+        color={Colors.white}
+        size={40}
+        disabled={!isEnabled}
+        onPress={() => {}}
+      />
+      <Countdown testID={`${testID}.Countdown`}>
+        {getMinSec(countdown)}
+      </Countdown>
+    </TimerContainer>
+  );
+};
+
+export default Timer;
+
+const Countdown = styled.Text`
+  color: ${Colors.white};
+  font-size: 36px;
+`;
+
+const TimerContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Hourglass = styled(IconButton)``;
+
+const getIconByStatus = (status: 'FULL' | 'RUNNING'): string => {
+  if (status === 'FULL') {
+    return 'timer-sand-full';
+  }
+
+  return 'timer-sand';
+};
--- src/screens/home/home.test.tsx
+++ src/screens/home/home.test.tsx
@@ -3,18 +3,39 @@ import React from 'react';
 import HomeScreen from './home';
 
 describe('Home', () => {
-  it(`Scenario: See purple, green, black timer
+  it(`Scenario: See purple timer on Start
     Given any
     When I load the App
-    Then I should see purple timer
-    And I should see green timer
-    And I should see black timer`, () => {
+    Then I should see purple hourglass at top
+    And I should see purple countdown 03:00`, () => {
     const {getByTestId} = render(<HomeScreen />);
 
-    expect(getByTestId('Purple.Hourglass.Bottom')).toBeDefined();
+    expect(getByTestId('Purple.Timer.Hourglass')).toBeDefined();
+    expect(getByTestId('Purple.Row').props.top).toEqual(0);
+    expect(getByTestId('Purple.Timer.Countdown').children).toEqual(['03:00']);
+  });
 
-    expect(getByTestId('Green.Hourglass.Bottom')).toBeDefined();
+  it(`Scenario: See green timer on Start
+    Given any
+    When I load the App
+    Then I should see green hourglass at top
+    And I should see green countdown 02:00`, () => {
+    const {getByTestId} = render(<HomeScreen />);
 
-    expect(getByTestId('Black.Hourglass.Bottom')).toBeDefined();
+    expect(getByTestId('Green.Timer.Hourglass')).toBeDefined();
+    expect(getByTestId('Green.Row').props.top).toEqual(0);
+    expect(getByTestId('Green.Timer.Countdown').children).toEqual(['02:00']);
+  });
+
+  it(`Scenario: See black timer on Start
+    Given any
+    When I load the App
+    Then I should see black hourglass at top
+    And I should see black countdown 00:40`, () => {
+    const {getByTestId} = render(<HomeScreen />);
+
+    expect(getByTestId('Black.Timer.Hourglass')).toBeDefined();
+    expect(getByTestId('Black.Row').props.top).toEqual(0);
+    expect(getByTestId('Black.Timer.Countdown').children).toEqual(['00:45']);
   });
 });
